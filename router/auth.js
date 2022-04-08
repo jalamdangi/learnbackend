8d094a959b06a491327da8d7b5bc552230c5e337
Jalam Dangi <JalamD@bpl.netlink.com>
2022-04-08 12:44:35 +0530

    password hashing done

--- router/auth.js
+++ router/auth.js
@@ -1,53 +1,61 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 require('../db/conn');
 const Users = require('../models/userSchema');
 
-router.get("/", (req,res) =>{
+router.get("/", (req, res) => {
     res.send("hello form the server");
 })
 
 // Register Api
-router.post("/register", async (req,res) =>{
-    const{name, email, phone, password, cpassword} = req.body;
-    if(!name || !email || !phone || !password || !cpassword){
-        return res.status(422).json({error:"All fields are required"}); 
-    }
-    try{
-        const userExist = await Users.findOne({email:email})
-        if(userExist){
-            return res.status(422).json({error:"Email already exist"}); 
-        }else if(password != cpassword){
-            return res.status(422).json({error:"Password are not matching"}); 
+router.post("/register", async (req, res) => {
+    const { name, email, phone, password, cpassword } = req.body;
+    if (!name || !email || !phone || !password || !cpassword) {
+        return res.status(422).json({ error: "All fields are required" });
+    } else {
+        try {
+            const userExist = await Users.findOne({ email: email })
+            if (userExist) {
+                return res.status(422).json({ error: "Email already exist" });
+            } else if (password != cpassword) {
+                return res.status(422).json({ error: "Password are not matching" });
+            }
+            else {
+                const User = new Users({ name, email, phone, password, cpassword })
+                // password hashing
+                await User.save();
+                res.status(201).json({ message: "user register successfully" });
+                console.log('success');
+            }
+        } catch (err) {
+            console.log(err);
         }
-        else{   
-            const User = new Users({name, email, phone, password, cpassword})
-            // password hashing
-            await User.save();
-            res.status(201).json({message:"user register successfully"});
-            console.log('success');
-        }
-    }catch(err){
-        console.log(err);
     }
 })
 
 
 // Login Api
-router.post("/login", async(req,res) =>{
-    try{
-        const {email,password} = req.body;
-        if(!email || !password){
-            return res.status(400).json({message: "Please fill both fields"});
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ message: "Please fill both fields" });
+        } else {
+            const emailExist = await Users.findOne({ email: email });
+            if (emailExist) {
+                const isMatch = await bcrypt.compare(password, emailExist.password);
+                if (isMatch) {
+                    res.json("Login successfull")
+                }
+                else { res.json("wrong password") }
+                //console.log(emailExist);
+            } else {
+                res.json({ message: "email not found" })
+            }
         }
-        const emailExist = await Users.findOne({email:email});
-        if(emailExist){
-            res.json({message:"login successfull"})
-            //console.log(emailExist);
-        }
-        else{res.json({message:"email not found"})}
-    }catch(err){
+    } catch (err) {
         console.log(err);
     }
 })
@@ -56,15 +64,15 @@ router.post("/login", async(req,res) =>{
 
 
 
-router.get("/about", (req,res) =>{
+router.get("/about", (req, res) => {
     res.send("hello form the about server");
 })
 
-router.get("/contact", (req,res) =>{
+router.get("/contact", (req, res) => {
     res.send("hello form the contact server");
 })
 
-router.get("/signin", (req,res) =>{
+router.get("/signin", (req, res) => {
     res.send("hello form the signin server");
 })
 
